6bc818b67659b381a0354b630667eddb3cb2c216
hu553in <hasanluckyman@gmail.com>
2018-08-11 22:23:13 +0600

    Unity3d fucked my brain so hard

--- Assets/Source/ClickHandlerScript.cs
+++ Assets/Source/ClickHandlerScript.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ClickHandlerScript : MonoBehaviour {
-	public void handleClick() {
+	public void onClick() {
         SceneManager.LoadSceneAsync("HistoryScene", LoadSceneMode.Single);
 	}
 }
