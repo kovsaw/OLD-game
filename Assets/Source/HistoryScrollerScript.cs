bf6e776d5ddfca9aea2a531f1d0e7565d138ec9a
Ruslan <37346043+hu553in@users.noreply.github.com>
2018-08-12 02:33:19 +0600

    designed pretty TitleScreen, implemented SW-like history scrolling on the HistoryScreen

---
+++ Assets/Source/HistoryScrollerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HistoryScrollerScript : MonoBehaviour {
+    public void Update () {
+        float scrollSpeed = 20;
+
+        // get current position of parent GameObject
+        Vector3 position = transform.position;
+
+        // get vector pointing into the distance
+        Vector3 localVectorUp = transform.TransformDirection(0, 1, 0);
+
+        // move the text object into the distance to give our 3D scrolling effect
+        position += localVectorUp * scrollSpeed * Time.deltaTime;
+        transform.position = position;    
+	}
+}
