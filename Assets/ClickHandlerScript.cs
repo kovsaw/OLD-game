60157d0910a80b23724168c170d7ad56a4861f99
hu553in <hasanluckyman@gmail.com>
2018-08-11 19:41:19 +0600

    created a button which makes the transition between scenes, remaked project

---
+++ Assets/ClickHandlerScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickHandlerScript : MonoBehaviour {
+	public void handleClick() {
+        SceneManager.LoadSceneAsync("HistoryScene", LoadSceneMode.Single);
+	}
+}
